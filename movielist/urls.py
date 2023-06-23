64f4feb3f01f51571a8c92076868de097d1fb3b2
linkdin-7 <56730903+linkdin-7@users.noreply.github.com>
2023-06-23 20:57:06 +0530

    Update urls.py

--- movielist/urls.py
+++ movielist/urls.py
@@ -7,8 +7,8 @@ from .views import MovieList,CollectionView,CollectionUUIDView,RequestCountView
 
 urlpatterns = [
     path('movies/',MovieList.as_view(),name='movie-list'),
-    path('collection/',CollectionView.as_view()),
-    path('collection/<collection_uuid>/',CollectionUUIDView.as_view()),
+    path('collection/',CollectionView.as_view(),name='collectionview'),
+    path('collection/<collection_uuid>/',CollectionUUIDView.as_view(),name='collectionuuidview'),
     path('request-count/', RequestCountView.as_view(), name='updaterequestCount'),
     path('request-count/reset/',RequestCountView.as_view(), name='resetrequestCount')
     
